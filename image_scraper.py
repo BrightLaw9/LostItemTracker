043522bbe8413c3a89038e0b527382fe0d9535dd
BrightLaw9 <46764863+BrightLaw9@users.noreply.github.com>
2025-02-16 18:59:20 -0500

    Add inference script, pytorch model

--- image_scraper.py
+++ image_scraper.py
@@ -2,11 +2,14 @@ import bs4
 import requests
 import os
 
-NUM_IMAGE_SCRAPE = 20
-images_dir = 'images2'
+NUM_IMAGE_SCRAPE = 30
+images_dir = 'images4'
 def scrape_and_download_images(image_description, image_class): 
     # Get the image search results page
-    res = requests.get('https://www.google.com/search?q=' + 'unattended ' + image_description + 'without a person' + '&source=lnms&tbm=isch')
+    res = requests.get('https://www.google.com/search?q=' #+ 'unattended ' 
+                       + image_description + 
+                       #'without a person' + 
+                       '&source=lnms&tbm=isch')
     res.raise_for_status()
 
     # Parse the page
@@ -33,21 +36,23 @@ def scrape_and_download_images(image_description, image_class):
                     image_file.write(chunk)
             count += 1
 
-scrape_and_download_images('backpacks at airport', 'backpacks')
-scrape_and_download_images('backpacks at school', 'backpacks')
-scrape_and_download_images('backpacks', 'backpacks')
-scrape_and_download_images('backpacks on bench', 'backpacks')
-scrape_and_download_images('backpacks at restaraunt', 'backpacks')
+#scrape_and_download_images('backpacks at airport', 'backpacks')
+#scrape_and_download_images('backpacks at school', 'backpacks')
+#scrape_and_download_images('backpacks white background', 'backpacks')
+#scrape_and_download_images('backpacks on bench', 'backpacks')
+#scrape_and_download_images('backpacks at restaraunt', 'backpacks')
 #scrape_and_download_images('wallets in restaraunt seat', 'wallets')
 #scrape_and_download_images('wallets at cash register counter', 'wallets')
 #scrape_and_download_images('smartphones on restaraunt seats', 'phones')
-scrape_and_download_images('baggage unattended at airport', 'baggage')
-scrape_and_download_images('baggage', 'baggage')
+#scrape_and_download_images('baggage unattended at airport', 'baggage')
+#scrape_and_download_images('baggage no background', 'baggage')
 #scrape_and_download_images('smartphones on a bench', 'phones')
-scrape_and_download_images('purse on a bench', 'purse')
-scrape_and_download_images('purse', 'purse')
+#scrape_and_download_images('purse on a bench', 'purse')
+#scrape_and_download_images('purse no background', 'purse')
 #scrape_and_download_images('purse on a restaraunt seat', 'purse')
-scrape_and_download_images('water bottle', 'water bottle')
+#scrape_and_download_images('water bottle no background', 'water bottle')
+scrape_and_download_images('phones white background', 'phones')
+scrape_and_download_images('phones no background', 'phones')
 #scrape_and_download_images('laptop on a bench alone', 'laptop')
 #scrape_and_download_images('water bottle on a desk with no hands or person', 'water bottle')
 #scrape_and_download_images('phone charger on a bench with no person', 'charger')  
